f3aca25f8c5e57dca91ce3065254fa1000859046
Sebastian Torres <storresy@innventto.com>
2020-04-10 18:05:25 -0500

    Innitial commit

---
+++ lib/excel_serializer.rb
@@ -0,0 +1,6 @@
+require "excel_serializer/version"
+
+module ExcelSerializer
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/excel_serializer/version.rb
@@ -0,0 +1,3 @@
+module ExcelSerializer
+  VERSION = "0.1.0"
+end
